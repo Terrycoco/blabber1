e19bb83f4cefff0768e72ce4f8434d8078b1f138
Terry Marr <terrymarr280@gmail.com>
2015-10-31 16:44:34 -0700

    return blabs from api

--- client/components/blabs/View.jsx
+++ client/components/blabs/View.jsx
@@ -5,16 +5,17 @@ module.exports = React.createClass({
   getInitialState: function() {
     return { data: [] };  //empty array to start
   },
+
   componentDidMount: function() {
-    this.setState({ data:
-      [
-      {id: 2,
-      content: "and another fake blab"},
-      {id:1,
-      content: "this is a fake blab"}
-      ]
-    });
+   this.readBlabsFromAPI();
   },
+
+   readBlabsFromAPI: function() {
+     this.props.readFromAPI(this.props.origin + '/blabs', function(blabs) {
+       this.setState({data: blabs});
+     }.bind(this));
+   },
+
   render: function() {
     return (
       <div className="blabs-view">
