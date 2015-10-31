521c2cc07051c9fa8e611932c847a295ea36e8fc
Terry Marr <terrymarr280@gmail.com>
2015-10-31 15:39:47 -0700

    set up server to allow CORS

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  #any request with OPTIONS method send to preflight code
+  match '*all', to: 'application#preflight', via: [:options]
 
   # blabs#index  /blabs
   resources :blabs, only: [:index]
